a12340e779f4f2411267eb0fa0df3ddc25f03e75
Ahmed Almogtaba <mogtabaaa2013@gmail.com>
2019-12-24 13:59:19 +0200

    first commit

---
+++ Desktop/resortapptwo/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Home from "./Pages/Home";
+import Rooms from "./Pages/Rooms";
+import SingleRoom from "./Pages/SingleRoom";
+import Error from "./Pages/Error";
+import { Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rooms/" component={Rooms} />
+        <Route exact path="/rooms/:slug" component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
+    </>
+  );
+}
+
+export default App;
---
+++ Desktop/resortapptwo/src/Contentful.js
@@ -0,0 +1,5 @@
+import { createClient } from "contentful";
+export default createClient({
+  space: "lt5q9u8mj497",
+  accessToken: "jy6-JMgLfM-UcDpvkT8-AlYebiw5yJqYfH2itr0C_tI"
+});
